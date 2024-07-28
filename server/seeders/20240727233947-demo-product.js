b06b9d18d9323f63c3cc14abb81654c1542b370e
danio <daniobregon85@gmail.com>
2024-07-28 19:41:40 -0300

    Adds product's model, migration and seeder

---
+++ server/seeders/20240727233947-demo-product.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Products', [
+      {
+        marca: 'Nike',
+        modelo: 'Air',
+        descripcion: 'No description',
+        precio: 3025.5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Products', { modelo: 'Air' }, {});
+  }
+};
