f1ba496962938baa412cfe1c9a908274f4adedd5
danio <daniobregon85@gmail.com>
2024-09-05 15:46:20 -0300

    Agrega cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); // Agrega esta línea
 const routes = require('../routes');
 const db = require('../models');
 const app = express();
@@ -6,6 +7,7 @@ const PORT = process.env.SERVER_PORT || 5000;
 
 app.use(express.json());
 app.use(routes);
+app.use(cors()); // Agrega esto para habilitar CORS
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
