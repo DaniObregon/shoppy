0544dacd1b7e82db20e81340fe5411d6d7349900
danio <daniobregon85@gmail.com>
2024-09-11 12:53:17 -0300

    Agrega BrowserRouter en App.jsx. Centraliza componentes actuales en MainPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-import { Header } from "./components/Header";
-import { Carousel } from "./components/Carousel";
-import { ProductList } from "./components/ProductList";
-import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MainPage } from "./pages/MainPage"; // Tu única página por ahora
 
 export const App = () => {
   return (
-    <>
-      <Header />;
-      <Navbar />
-      <Carousel />
-      <ProductList />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* Aquí puedes agregar más rutas en el futuro */}
+      </Routes>
+    </Router>
   );
 };
