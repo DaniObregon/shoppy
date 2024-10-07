2c26287277929267668684ed52112e7de259c289
danio <daniobregon85@gmail.com>
2024-10-07 14:29:19 -0300

    Refactoring: Centraliza imagenes slides del Carousel en el archivo carouselSlides.js

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,21 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { slides } from "../data/carouselSlides";
 
 export const Carousel = () => {
-  const slides = [
-    {
-      img: "https://www.brooksrunning.com/on/demandware.static/-/Library-Sites-BrooksRunningShared/default/dw26f51a29/cms-content/Project/ADT/Brooks-Running/EMEA/Blog/2023/April/best-running-shoes-for-knee-pain/S23_RHB_knee_Hero_XL.jpg",
-    },
-    {
-      img: "https://hips.hearstapps.com/hmg-prod/images/run-reebok-running-shoes-64e3beadbe692.jpg",
-    },
-    {
-      img: "https://brand.assets.adidas.com/capi/enUS/Images/1839569-seo-how-to-break-in-running-shoes-body-image-2_221-681798.jpg",
-    },
-    {
-      img: "https://portcrossfit.com/wp-content/uploads/2023/08/run-nike-running-shoes-646cdd1a19c41-1200x600.jpg",
-    },
-  ];
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const slidesCount = slides.length;
