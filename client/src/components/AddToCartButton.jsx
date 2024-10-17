66626779fa0160cd60a8adda70e140a00bc612f4
danio <daniobregon85@gmail.com>
2024-10-17 14:14:07 -0300

    Agrega tests unitarios para componente AddToCartButton

--- client/src/components/AddToCartButton.jsx
+++ client/src/components/AddToCartButton.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { chakra } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
-
-export const AddToCartButton = ({ id, onClick }) => {
+export const AddToCartButton = ({ id }) => {
   // Obtener el role_id desde el estado de Redux
   const role_id = useSelector((state) => state.userInfo.role_id);
 
+  // Evita la propagación del clic hacia el contenedor de la tarjeta
   const handleAddToCart = (event) => {
-    event.stopPropagation(); // Evita la propagación del clic hacia el contenedor de la tarjeta
+    event.stopPropagation();
 
     // Lógica para agregar el producto al carrito
     if (role_id === 2) {
