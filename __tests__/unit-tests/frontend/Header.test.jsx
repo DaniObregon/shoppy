aa68f1e8fe9c7745d8dc0a3e538ddb4d9b068caf
danio <daniobregon85@gmail.com>
2024-11-05 15:45:35 -0300

    Agrega tests unitarios para el componente Header

---
+++ __tests__/unit-tests/frontend/Header.test.jsx
@@ -0,0 +1,32 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Header } from "../../../client/src/components/Header";
+
+describe("Header component", () => {
+  test("Should render Header component with expected structure", () => {
+    render(
+      <ChakraProvider>
+        <Header />
+      </ChakraProvider>
+    );
+
+    // Verifica que el contenedor principal del Header está presente
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
+
+    // Verifica que el header contiene tres elementos de texto
+    const textElements = screen.getAllByText(/./); // Selector genérico para cualquier texto
+    expect(textElements.length).toBe(3);
+  });
+
+  test("should match snapshot", () => {
+    const { asFragment } = render(
+      <ChakraProvider>
+        <Header />
+      </ChakraProvider>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
