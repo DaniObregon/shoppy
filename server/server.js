0bbbd1f4f7015caf076fa2e1d26fc883905cfc19
danio <daniobregon85@gmail.com>
2024-12-02 09:31:31 -0300

    Agrega lods de conexion con la base de datos en server.js

--- server/server.js
+++ server/server.js
@@ -13,10 +13,16 @@ if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
     try {
+      console.log('Intentando conectar a la base de datos...');
+      console.log('URL de conexión:', process.env.DATABASE_URL);
+      await db.sequelize.authenticate(); // Prueba de conexión
+      console.log("Conexión a la base de datos establecida correctamente");
       await db.sequelize.sync();
       console.log("Database synchronized");
     } catch (error) {
       console.error("Error executing seeders:", error);
+      console.error("Error de conexión a la base de datos:", error);
+      console.error("Detalles completos del error:", JSON.stringify(error, null, 2));
     }
   });
 }
