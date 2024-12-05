e32c9fc07fe50402542194920cd7cef0a185c12d
danio <daniobregon85@gmail.com>
2024-12-05 09:30:30 -0300

    Agrega log para verificar la variable FIREBASE_SERVICE_ACCOUNT

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -5,7 +5,9 @@ var serviceAccount;
 // Verifica si estás en un entorno de producción o desarrollo
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   // Si la variable de entorno existe, la parseamos desde una cadena JSON
+  console.log("FIREBASE_SERVICE_ACCOUNT:::" + JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 } else {
   // Si no existe la variable de entorno, carga el archivo localmente (para desarrollo)
   serviceAccount = require("../things/shoppy-153a0-firebase-adminsdk-r3s3l-a96e044339.json");
