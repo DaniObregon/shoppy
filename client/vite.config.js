70efb05d671f22115b6cc6213a3fe85db1800222
danio <daniobregon85@gmail.com>
2024-12-09 12:44:55 -0300

    Agrega puerto especifico en vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,20 +8,27 @@ dotenv.config();
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['src/config/firebaseConfig.js'], // Evita empaquetar el archivo
+      external: ["src/config/firebaseConfig.js"], // Evita empaquetar el archivo
     },
   },
   plugins: [react()],
   server: {
+    port: process.env.PORT || 3000,
     proxy: {
       "/api": {
         // Ajustar el target dependiendo del entorno
-        target: process.env.NODE_ENV === 'production' ? 'https://shoppy-g5bu.onrender.com' : `http://localhost:${process.env.PORT}`,
+        target:
+          process.env.NODE_ENV === "production"
+            ? "https://shoppy-g5bu.onrender.com"
+            : `http://localhost:${process.env.PORT}`,
         secure: false,
         changeOrigin: true,
       },
       "/admin-api": {
-        target: process.env.NODE_ENV === 'production' ? 'https://shoppy-g5bu.onrender.com' : `http://localhost:${process.env.PORT}`,
+        target:
+          process.env.NODE_ENV === "production"
+            ? "https://shoppy-g5bu.onrender.com"
+            : `http://localhost:${process.env.PORT}`,
         secure: false,
       },
       port: process.env.PORT,
