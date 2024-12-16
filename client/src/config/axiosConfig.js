ee1cdd3d14e54972cbf3d5788d48660055fc80b3
danio <daniobregon85@gmail.com>
2024-12-16 08:47:31 -0300

    Agrega interceptor a la configuracion de axios para enviar el token en cada solicitud

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../redux/store";
 
 const api = axios.create({
   baseURL:
@@ -6,5 +7,19 @@ const api = axios.create({
       ? "https://shoppy-g5bu.onrender.com" // URL base para producción
       : "http://localhost:5000", // URL base para desarrollo
 });
+
+// Interceptor para agregar el token a cada solicitud
+api.interceptors.request.use(
+  (config) => {
+    const token = store.getState().userInfo.token; // Obtener el token desde Redux
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Agregar token en el header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
