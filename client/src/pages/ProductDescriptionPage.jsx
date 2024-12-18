067b28e7413ee172de7acdb0e9b5a909269c17a0
danio <daniobregon85@gmail.com>
2024-12-18 12:35:29 -0300

    Reorganiza los componentes de ProductDescriptionPage

--- client/src/pages/ProductDescriptionPage.jsx
+++ client/src/pages/ProductDescriptionPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "../components/Header";
 import { Navbar } from "../components/Navbar";
-import { ProductDetails } from "../components/ProductDetails";
+import { ProductDetails } from "../components/product-description-page/ProductDetails";
 
 export const ProductDescriptionPage = () => {
   return (
